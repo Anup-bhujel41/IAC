f1966b647cf5f9ccd1bd27a036b00f7a2740ac1d
DarkWhit33 <anupbhujel41@gmail.com>
2024-07-13 15:50:54 +0545

    ECR code

--- ecr/main.tf
+++ ecr/main.tf
@@ -1,3 +1,8 @@
-resource "aws_ecr_repository" "backend_services" {
-  name = ""
+resource "aws_ecr_repository" "backend_services_ecr" {
+  name                 = "backend_services_ecr"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
--- ecr/outputs.tf
+++ ecr/outputs.tf
@@ -1,3 +1,4 @@
-output "name" {
-
+output "backend_services_ecr_arn" {
+  description = ""
+  value       = aws_ecr_repository.backend_services_ecr
 }
